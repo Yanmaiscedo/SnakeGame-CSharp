4211fdb240a1e0c590c50a969f0623a1ef2c2fc6
Yan <134351027+Yanmaiscedo@users.noreply.github.com>
2025-03-16 18:32:32 -0300

    Feat: Variables

--- SnakeGame.cs
+++ SnakeGame.cs
@@ -3,5 +3,14 @@ using System.Threading;
 
 class SnakeGame
 {
+    static bool gameOver;
+    const int width = 40;
+    const int height = 27;
+    static int x, y, fruitX, fruitY, score;
+    static int[] tailX = new int[100], tailY = new int[100];
+    static int nTail;
+    static Direction dir;
+
+    enum Direction { STOP = 0, LEFT, RIGHT, UP, DOWN }
 
 }
